6f97c78f5a7f1bcff35a5dac7d997902474ad1e7
Hiagun1 <hiagobrenha75@gmail.com>
2020-10-23 23:08:54 -0300

    CRUD Scaffolding

---
+++ NetMVC/Data/NetMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NetMVC.Models;
+
+namespace NetMVC.Data
+{
+    public class NetMVCContext : DbContext
+    {
+        public NetMVCContext (DbContextOptions<NetMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<NetMVC.Models.Department> Department { get; set; }
+    }
+}
