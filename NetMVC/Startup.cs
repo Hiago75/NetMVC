f2ca69ca5719967e8db6a9cef43ad78a0db4d5bc
Hiagun1 <hiagobrenha75@gmail.com>
2020-10-27 21:17:08 -0300

    Base de dados populada para testes

--- NetMVC/Startup.cs
+++ NetMVC/Startup.cs
@@ -38,14 +38,17 @@ namespace NetMVC
 
             services.AddDbContext<NetMVCContext>(options =>
                     options.UseMySql(Configuration.GetConnectionString("NetMVCContext"), builder => builder.MigrationsAssembly("NetMVC")));
+
+            services.AddScoped<SeedingService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
             }
             else
             {
