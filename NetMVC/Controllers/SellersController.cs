815971cf1ece05f00123c5a84118d79d0293dcbb
Hiagun1 <hiagobrenha75@gmail.com>
2020-10-30 17:24:07 -0300

    Pagina de erro personalizada adicionada, tratamento em geral melhorado

--- NetMVC/Controllers/SellersController.cs
+++ NetMVC/Controllers/SellersController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -45,11 +46,11 @@ namespace NetMVC.Controllers
         public IActionResult Delete(int? id)
         {
             if (id == null)
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "ID não fornecido"});
 
             var obj = _sellerService.FindById(id.Value);
             if (obj == null)
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "ID não encontrado" });
 
             return View(obj);
         }
@@ -65,11 +66,11 @@ namespace NetMVC.Controllers
         public IActionResult Details(int? id)
         {
             if(id == null)
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "ID não fornecido" });
 
             var obj = _sellerService.FindById(id.Value);
             if (obj == null)
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "ID não encontrado" });
 
             return View(obj);
         }
@@ -78,12 +79,12 @@ namespace NetMVC.Controllers
         {
             if (id == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "ID não fornecido" });
             }
             var obj = _sellerService.FindById(id.Value);
             if (obj == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "ID não encontrado" }); ;
             }
 
             List<Department> departments = _departmentService.FindAll();
@@ -97,20 +98,26 @@ namespace NetMVC.Controllers
         {
             if(id != seller.Id)
             {
-                return BadRequest();
+                return RedirectToAction(nameof(Error), new { message = "Os numeros de ID não são compatíveis" }); ;
             }
             try { 
             _sellerService.Update(seller);
             return RedirectToAction(nameof(Index));
             }
-            catch (NotFoundException)
+            catch (ApplicationException e)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = e.Message }) ;
             }
-            catch (DbConcurrencyException)
+        }
+
+         public IActionResult Error(string message)
+        {
+            var viewModel = new ErrorViewModel
             {
-                return BadRequest();
-            }
+                Message = message,
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            };
+            return View(viewModel);
         }
     }
 }
