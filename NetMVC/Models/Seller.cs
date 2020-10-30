1199c56a984e0b84a438b3b3cb35c5957bdf8d4e
Hiagun1 <hiagobrenha75@gmail.com>
2020-10-30 18:11:22 -0300

    Aprimorado sistema de validação

--- NetMVC/Models/Seller.cs
+++ NetMVC/Models/Seller.cs
@@ -8,20 +8,31 @@ namespace NetMVC.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} é um campo obrigatório")]
+        [StringLength(255, MinimumLength = 3, ErrorMessage = "O tamanho do nome precisa conter {2}-{1} caracteres")]
         [Display(Name = "Nome")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "{0} é um campo obrigatório")]
+        [EmailAddress(ErrorMessage = "Insira um email válido")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
-        [Display(Name = "Data de nascimento")]
+
+        [Required(ErrorMessage = "Data de aniversário é um campo obrigatório")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
-        [Display(Name = "Salário Base")]
+
+        [Required(ErrorMessage = "Salário base é um campo obrigatório")]
+        [Range(954.00, 50000.00, ErrorMessage ="{0} precisa estar entre {1} e {2}")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
-        [Display(Name = "Departamento")]
+
         public Department Department { get; set; }
+
         public int DepartmentId { get; set; }
+
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller()
